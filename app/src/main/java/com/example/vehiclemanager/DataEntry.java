aa0cfe8cc4ebcdc8f22a61886daab92df95ad9c8
Kavin S <51694233+Kavin511@users.noreply.github.com>
2020-08-28 23:26:10 +0530

    1st stage

---
+++ app/src/main/java/com/example/vehiclemanager/DataEntry.java
@@ -0,0 +1,21 @@
+package com.example.vehiclemanager;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+class DataEntry extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_data_entry, container, false);
+    }
+}
